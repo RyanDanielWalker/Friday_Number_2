562bf1f1461665d5af8771d2c11bb7af7f05d4b7
Ryan Walker <ryandanielwalker@gmail.com>
2021-03-26 16:48:28 -0700

    Prepare to push to GitHub for submission to Epicenter.

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,13 @@
 // USER INTERFACE LOGIC
 $(document).ready(function(){
   $("form#survey").submit(function(event){
+    event.preventDefault();
     const number1 = parseInt($("#number1").val());
     const number2 = parseInt($("#number2").val());
     const number3 = parseInt($("#number3").val());
     const number4 = parseInt($("#number4").val());
     const number5 = parseInt($("#number5").val());
     const result = (number1 + number2 + number3 + number4 + number5);
-    
-  
-    
     if (result <= 5) {
       $("#ruby").fadeIn() + $("#surveyCard").fadeOut();
     } else if (result > 5 && result <= 10) {
@@ -25,27 +23,29 @@ $(document).ready(function(){
     } else if (result > 10) {
       $("#cSharp").fadeIn() + $("#surveyCard").fadeOut();
     }
-
-    
-  
-    event.preventDefault();
-
   });
-
   $("#name").submit(function(event){
     const name = $("input#person1").val();
-    
-    $(".userName").text(name);
-    
+    event.preventDefault();
     if (name) {
       $("#surveyCard").fadeIn() + $("#name").fadeOut();
     }
     else {
       alert("Please enter a name!");
     }
-    
-    event.preventDefault();
+    $(".userName").text(name);
   });
+    
+  
+  
+
+    
+  
+
+
+    
+    
+    
   
   
 
