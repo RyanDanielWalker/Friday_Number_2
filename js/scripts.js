55c33755f760c839ecfe65364454c84a00a34dc4
Ryan Walker <ryandanielwalker@gmail.com>
2021-03-26 09:43:53 -0700

    Add 5 survey questions and select boxes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
+// function add() {
+//   return 
+// }
+
+
+
+
+
 // USER INTERFACE LOGIC
 $(document).ready(function(){
   $("form#survey").submit(function(){
     event.preventDefault();
-    const number1 = parseInt($("#disco").val());
+    const number1 = parseInt($("#number1").val());
+    const number2 = parseInt($("#number2").val());
+    const number3 = parseInt($("#number3").val());
+    const number4 = parseInt($("#number4").val());
+    const number5 = parseInt($("#number5").val());
 
-    $("#output").text(number1);
+    
+
+
+    
+    
+    
+    
+    $("#output").text();
   });
-
 
 });
