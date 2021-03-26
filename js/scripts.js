74632175aeca484b7aa777562bc6d4f8a9330d9c
Ryan Walker <ryandanielwalker@gmail.com>
2021-03-26 16:10:32 -0700

    Add functions for displaying correct surveys based on sums

--- js/scripts.js
+++ js/scripts.js
@@ -15,32 +15,37 @@ $(document).ready(function(){
     const number4 = parseInt($("#number4").val());
     const number5 = parseInt($("#number5").val());
     const result = (number1 + number2 + number3 + number4 + number5);
+    const surveyFade = $("#surveyCard").fadeOut();
   
     
-    if (result > 1) {
-      $("#ruby").fadeIn() + $("#survey").fadeOut();
+    if (result <= 5) {
+      $("#ruby").fadeIn() + surveyFade;
+    } else if (result > 5 && result <= 10) {
+      $("#javascript").fadeIn() + surveyFade;
+    } else if (result > 10) {
+      $("#cSharp").fadeIn() + surveyFade ;
     }
 
     
-    
+  
     event.preventDefault();
 
   });
 
-  $("#name").submit(function(event){
-    const name = $("input#person1").val();
+  // $("#name").submit(function(event){
+  //   const name = $("input#person1").val();
     
-    $(".userName").text(name);
+  //   $(".userName").text(name);
     
-    if (name) {
-      $("#survey").fadeIn() + $("#name").fadeOut();
-    }
-    else {
-      alert("Please enter a name!");
-    }
+  //   if (name) {
+  //     $("#survey").fadeIn() + $("#name").fadeOut();
+  //   }
+  //   else {
+  //     alert("Please enter a name!");
+  //   }
     
-    event.preventDefault();
-  });
+  //   event.preventDefault();
+  // });
   
   
 
